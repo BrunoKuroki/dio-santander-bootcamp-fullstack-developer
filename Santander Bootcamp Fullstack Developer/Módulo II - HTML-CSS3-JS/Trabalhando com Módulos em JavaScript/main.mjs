e25da569932311af870b8e306b94cd44499f3a2d
BrunoKuroki <bruno_works@outlook.com>
2022-07-27 18:45:44 -0300

    resumo + atividade

---
+++ Santander Bootcamp Fullstack Developer/Módulo II - HTML-CSS3-JS/Trabalhando com Módulos em JavaScript/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Bruno', 24));
+
+console.log(mostraCidade('Bruno', 'Araçatuba'));
+
+console.log(mostraHobby('Bruno', 'Ouvir música'));
